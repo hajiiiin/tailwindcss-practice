6a9c5067fed56514803c429126c3c3b7ca5ce3ac
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2025-01-17 15:07:02 +0900

    feat: HeroUI select import
    custom selector 만들기

---
+++ app/custom-selector/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Select, SelectSection, SelectItem } from "@heroui/select";
+
+function CustomSelector() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default CustomSelector;
